f66312bc4957f128016f996c156223969ac163ce
duyhdgch18823 <62170667+duyhdgch18823@users.noreply.github.com>
2020-04-28 14:55:05 +0700

    alola

--- product.js
+++ product.js
@@ -2,34 +2,11 @@ const express = require('express');
 var router = express.Router();
 
 
-// const bodyParser = require('body-parser');
-// const app = express();
-// const multer = require('multer');
-// fs = require('fs-extra');
-// app.use(bodyParser.urlencoded({ extended: true }));
-// var router = express.Router();
-
-
 var MongoClient = require('mongodb').MongoClient;
-ObjectId = require('mongodb').ObjectId;
 
 
 // var url = 'mongodb://localhost:27017';
 var url = 'mongodb+srv://DuyHD:duy106hn@cluster0-knunn.azure.mongodb.net/test?retryWrites=true&w=majority';
-
-
-// var storage = multer.diskStorage({
-//     // Upload file vao thu muc 'upload'
-//     destination: function (req, file, cb) {
-//         cb(null, 'uploads')
-//     },
-//     // Dat ten file
-//     filename: function (req, file, cb) {
-//         cb(null, file.fieldname + '-' + Date.now())
-//     }
-// });
-
-// var upload = multer({ storage: storage });
 
 
 
@@ -49,7 +26,7 @@ router.get('/insert', (req, res) => {
 })
 
 
-router.post('/insert', /*upload.single('picture'),*/  async (req, res) => {
+router.post('/insert', async (req, res) => {
     let client = await MongoClient.connect(url);
     let dbo = client.db("ATNToy");
 
@@ -58,15 +35,7 @@ router.post('/insert', /*upload.single('picture'),*/  async (req, res) => {
     let color = req.body.color;
     let price = req.body.price;
 
-    // var img = fs.readFileSync(req.file.path);
-    // var encode_image = img.toString('base64');
-
-    // var finalImg = {
-    //     contentType: req.file.mimetype,
-    //     image: new Buffer(encode_image, 'base64')
-    // };
-
-    let newSP = { Name: name, Category: cate, Color: color, Price: price, /*Image: finalImg*/ };
+    let newSP = { Name: name, Category: cate, Color: color, Price: price };
     await dbo.collection("Product").insertOne(newSP);
 
     let results = await dbo.collection("Product").find({}).toArray();
@@ -144,35 +113,5 @@ router.post('/search', async (req, res) => {
     let results = await dbo.collection("Product").find({ "Name": searchPrd }).toArray();
     res.render('allProduct', { product: results });
 })
-
-
-
-
-// image
-// Hien thi toan bo anh duoi dang id
-// router.get('/photos', (req, res) => {
-//     db.collection('ATNToy').find().toArray((err, result) => {
-
-//         const imgArray = result.map(element => element._id);
-//         console.log(imgArray);
-//         if (err) return console.log(err)
-//         res.send(imgArray)
-
-//     })
-// });
-
-
-// // Ham hien thi anh dua tren id cua chung
-// router.get('/photo/:id', (req, res) => {
-//     var filename = req.params.id;
-
-//     db.collection('ATNToy').findOne({ '_id': ObjectId(filename) }, (err, result) => {
-//         if (err) return console.log(err)
-//         res.contentType('image/jpeg');
-//         res.send(result.image.buffer)
-
-//     })
-// })
-
 
 module.exports = router;
